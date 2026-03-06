002a9bf1724fc6d57a64825f11f79629b7299654
George Balabanov <rnoctezuma@gmail.com>
2026-03-07 03:32:42 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ echo "================================="
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 steps=(
-  01_disk_partition.sh
+  01_disk.sh
   02_luks.sh
   03_btrfs.sh
   04_base_install.sh
@@ -21,7 +21,12 @@ for step in "${steps[@]}"; do
   echo "Running $step"
   echo "---------------------------------"
 
-  bash "$SCRIPT_DIR/steps/$step"
+  if [[ "$step" == "05_system_config.sh" ]]; then
+      cp "$SCRIPT_DIR/steps/$step" /mnt/root/
+      arch-chroot /mnt bash "/root/$step"
+  else
+      bash "$SCRIPT_DIR/steps/$step"
+  fi
 
   echo
   echo "$step completed."
